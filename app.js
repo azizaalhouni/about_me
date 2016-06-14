847574c11c857437b39ca8a457d367e7cfeb6c9b
aziza alhouni <azizaalhouni@yahoo.com>
2016-06-14 14:54:26 -0700

    first question finish

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('YEEEEAAAAA!!!!!!');
+'use strict';
+
+var userName = prompt('Greetings!!! what is your name>');
+alert('Welcome ' + userName +', glad you\'re here!');
+var answer1 = prompt('Does Sam have 3 cats?').toLowerCase();
+if(answer1 === 'yes' || answer1 === 'y'){
+  alert('Right! sam has felines named Buddy, Alistair, and Trillian');
+}else {
+  alert('Wrong, Sam actually does have 3 cats. Weird dude.');
+}
