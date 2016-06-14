e355504963575bbf83d65ee7e311efabfdb93222
aziza alhouni <azizaalhouni@yahoo.com>
2016-06-14 15:31:15 -0700

    edited the questions

--- app.js
+++ app.js
@@ -2,25 +2,25 @@
 
 var userName = prompt('Greetings!!! what is your name>');
 alert('Welcome ' + userName + ', glad you\'re here!');
-var answer1 = prompt('Does Sam have 3 cats?').toLowerCase();
+var answer1 = prompt('Does Aziza have a nickName?').toLowerCase();
 if(answer1 === 'yes' || answer1 === 'y'){
-  alert('Right! sam has felines named Buddy, Alistair, and Trillian');
+  alert('Right! aziza has a nickName which is zozo ');
 }else {
-  alert('Wrong, Sam actually does have 3 cats. Weird dude.');
+  alert('Wrong, Aziza actually does have a nickName which is zozo.');
 }
-var answer2 = prompt('Does Aziza has kids?').toLowerCase();
+var answer2 = prompt('Does Aziza have kids?').toLowerCase();
 if(answer2 === 'yes' || answer2 === 'y')
 {
   alert('Right! Aziza has two kids');
 }else{
-  alert('Wrong Aziza does have 2 kids');
+  alert('Wrong, Aziza does have 2 kids');
 }
 var answer3 = prompt('Does Aziza like Dancing').toLowerCase();
 if(answer3 === 'yes' || answer3 === 'y')
 {
   alert('Right! Aziza loves dancing');
 }else{
-  alert('Wrong! Aziza does like to dancing');
+  alert('Wrong! Aziza does like to dance');
 }
 var answer4 = prompt('Does Aziza live in Seattle').toLowerCase();
 if(answer4 === 'yes' || answer4 === 'y'){
